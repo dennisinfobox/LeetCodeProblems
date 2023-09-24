a8339f426a7b7b812c7d67b47184abd2b61ca80d
dennisinfobox <dennisinfobox@gmail.com>
2023-09-25 00:06:52 +0300

    704

---
+++ LeetCodeProblems/Solutions/Solution704.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCodeProblems.Solutions
+{
+    // https://leetcode.com/problems/binary-search/description/
+    public class Solution704
+    {
+        public int Search(int[] nums, int target)
+        {
+            var length = nums.Length;
+            return DoSearch(nums, target, 0, length - 1);
+        }
+
+        private int DoSearch(int[] container, int target, int startIndex, int endIndex)
+        {
+            if (startIndex == endIndex)
+            {
+                return container[startIndex] == target ? startIndex : -1;
+            }
+
+            if (endIndex - startIndex == 1)
+            {
+                return container[startIndex] == target ? startIndex : container[endIndex] == target ? endIndex : - 1;
+            }
+
+            var midIndex = startIndex + (endIndex - startIndex) / 2;
+
+            if (container[midIndex] == target)
+            {
+                return midIndex;
+            }
+
+            if (container[midIndex] > target)
+            {
+                return DoSearch(container, target, startIndex, midIndex);
+            }
+
+            return DoSearch(container, target, midIndex, endIndex);
+        }
+    }
+}
---
+++ LeetCodeProblemsTests/Solutions/Solution704Tests.cs
@@ -0,0 +1,24 @@
+﻿
+
+using LeetCodeProblems.Solutions;
+using NUnit.Framework;
+
+namespace LeetCodeProblemsTests.Solutions;
+
+public class Solution704Tests
+{
+
+    [TestCaseSource(nameof(GetTestCases))]
+    public void MaximumWealthTests(int[] nums, int target, int expected)
+    {
+        var sut = new Solution704();
+        Assert.AreEqual(expected, sut.Search(nums, target)); 
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCases()
+    {
+        yield return new TestCaseData( new[] { -1, 0, 3, 5, 9, 12 }, 9, 4);
+        yield return new TestCaseData( new[] { -1, 0, 3, 5, 9, 12 }, 2, -1);
+        // Add more test cases as needed
+    }
+}
