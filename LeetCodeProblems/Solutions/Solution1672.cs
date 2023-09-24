90284adcd7e7b0a1e178d2ec9a2128e2d9e444de
dennisinfobox <dennisinfobox@gmail.com>
2023-09-24 21:21:09 +0300

    Add project files.

---
+++ LeetCodeProblems/Solutions/Solution1672.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCodeProblems.Solutions
+{
+    public class Solution1672
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            return accounts.Max(r=>r.Sum());
+        }
+    }
+}
---
+++ LeetCodeProblemsTests/Solutions/Solution1672Tests.cs
@@ -0,0 +1,25 @@
+﻿
+
+using LeetCodeProblems.Solutions;
+using NUnit.Framework;
+
+namespace LeetCodeProblemsTests.Solutions;
+
+public class Solution1672Tests
+{
+
+    [TestCaseSource(nameof(GetTestCases))]
+    public void MaximumWealthTests(int[][] accounts, int expected)
+    {
+        var sut = new Solution1672();
+        Assert.AreEqual(expected, sut.MaximumWealth(accounts)); 
+    }
+
+    private static IEnumerable<TestCaseData> GetTestCases()
+    {
+        yield return new TestCaseData(new[]{ new[] { 1, 2, 3 }, new[] { 3, 2, 1 } }, 6);
+        yield return new TestCaseData(new[]{ new[] { 1,5 }, new[] { 7, 3 }, new[] { 3, 5 } }, 10);
+        yield return new TestCaseData(new[]{ new[] { 2,8,7 }, new[] { 7, 1, 3 }, new[] { 1, 9, 5 } }, 17);
+        // Add more test cases as needed
+    }
+}
