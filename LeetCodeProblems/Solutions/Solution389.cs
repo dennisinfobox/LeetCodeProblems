9400c3a476482460da0e19fb4ea4147d5014f7e4
dennisinfobox <dennisinfobox@gmail.com>
2023-10-03 10:37:27 +0300

    389

---
+++ LeetCodeProblems/Solutions/Solution389.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCodeProblems.Solutions;
+
+public class Solution389
+{
+    public char FindTheDifference(string s, string t)
+    {
+        var orderedList = s.OrderBy(r => r).ToList();
+            
+        foreach (var c in t)
+        {
+            var index = orderedList.BinarySearch(c);
+            if (index < 0)
+                return c;
+            orderedList.RemoveAt(index);
+        }
+        return '\0';
+    }
+}
---
+++ LeetCodeProblemsTests/Solutions/Solution389Tests.cs
@@ -0,0 +1,19 @@
+﻿
+
+using LeetCodeProblems.Solutions;
+using NUnit.Framework;
+
+namespace LeetCodeProblemsTests.Solutions;
+
+public class Solution389Tests
+{
+
+    [TestCase("abcd", "abcde", 'e')]
+    [TestCase("", "y", 'y')]
+    [TestCase("a", "aa", 'a')]
+    public void MaximumWealthTests(string s, string t, char expected)
+    {
+        var sut = new Solution389();
+        Assert.AreEqual(expected, sut.FindTheDifference(s, t)); 
+    }
+}
